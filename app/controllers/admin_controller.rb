2fb8490aa09918cf8d4f3c8e0d45b0d033680564
Caradr42 <carlosAdr2gv@gmail.com>
2019-11-02 14:28:25 -0600

    added admins functionality etc

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,26 +1,59 @@
 class AdminController < ApplicationController
-
-	# DAYS =["Lunes","Martes","Miercoles","Jueves","Viernes"]
-	# PROF = ["Pancho","Pacco","Pepe","Pedro"]
-	@@actividades = []
+	#@@actividades = []
 
 	def administrador
-		@acts = @@actividades
+		@acts = $actividades
 	end
 
+
 	def create
-		render plain: params[:actividad].inspect
-		@@actividades.push(Actividad.new(
+		#render plain: params[:actividad].inspect
+		index = $actividades.length
+		daysList = []
+		if params[:actividad]["Lu"] == '1'
+			daysList.push("Lunes")
+		end
+		if params[:actividad]["Ma"] == '1'
+			daysList.push("Martes")
+		end
+		if params[:actividad]["Mi"] == '1'
+			daysList.push("Miercoles")
+		end
+		if params[:actividad]["Ju"] == '1'
+			daysList.push("Jueves")
+		end
+		if params[:actividad]["Vi"] == '1'
+			daysList.push("Vieres")
+		end
+
+
+		for dia in daysList
+			$clases.push(Clase.new(
+				params[:actividad]["nombre"],
+				params[:actividad]["horaInicio"],
+				params[:actividad]["horaFin"],
+				dia,
+				params[:actividad]["profesor"],
+				true,
+				params[:actividad]["capacidad"],
+				index
+				))
+		end
+		
+		$actividades.push(Actividad.new(
 				params[:actividad]["nombre"], 
 				params[:actividad]["horaInicio"],
 				params[:actividad]["horaFin"],
-				params[:actividad]["Lu"],
-				params[:actividad]["Ma"],
-				params[:actividad]["Mi"],
-				params[:actividad]["Ju"],
-				params[:actividad]["Vi"],
+				params[:actividad]["Lu"] == '1',
+				params[:actividad]["Ma"] == '1',
+				params[:actividad]["Mi"] == '1',
+				params[:actividad]["Ju"] == '1',
+				params[:actividad]["Vi"] == '1',
 				params[:actividad]["profesor"],
+				params[:actividad]["capacidad"]
 				))
+		
+		redirect_to :admin => 'admin'
 	end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,58 @@
 class ApplicationController < ActionController::Base
-	
+	#Clase.new("Ciclismo","2:30","3:15","15 nov","Pedro","true"), 
+	#			Clase.new("Cintas","2:30","3:15","15 nov","Juan","true")
+	$clases	= []
+	$actividades = []
+
 	class Clase
-		attr_accessor :nombre, :horaInicio, :horaFin, :fecha, :profesor, :activa
-		def initialize(nombre, horaInicio, horaFin, fecha, profesor, activa)
+		attr_accessor :nombre, :horaInicio, :horaFin, :dia, :profesor, :activa, :capacidad, :activityIndex
+
+		def initialize(nombre, horaInicio, horaFin, dia, profesor, activa, capacidad, activityIndex)
 			@nombre = nombre
 			@horaInicio = horaInicio
 			@horaFin = horaFin
-			@fecha = fecha
+			@dia = dia
 			@profesor = profesor
 			@activa = activa
+			@capacidad = capacidad
+			@activityIndex = activityIndex
 		end
+
+		# @@clases = []
+
+		# def clases
+	 #   		@@clases
+		# end
+		# setter
+		# def clases (ololo)
+		#    @@clases = ololo
+		# end
 	end
 
 	class Actividad
-		attr_accessor :nombre, :horaInicio, :horaFin, :lu, :ma, :mi, :ju, :vi, :profesor
-		def initialize(nombre, horaInicio, horaFin, lu, ma, mi, ju, vi, profesor)
+		attr_accessor :nombre, :horaInicio, :horaFin, :lu, :ma, :mi, :ju, :vi, :profesor, :capacidad
+
+		def initialize(nombre, horaInicio, horaFin, lu, ma, mi, ju, vi, profesor, capacidad)
 			@nombre = nombre
 			@horaInicio = horaInicio
-			@horaFin = horaFin
+			@horaFin =  horaFin
 			@lu = lu
 			@ma = ma
 			@mi = mi
 			@ju = ju
 			@vi = vi
 			@profesor = profesor
+			@capacidad = capacidad
 		end
+
+		# @@actividades = []
+
+		# def actividades
+	 #   		@@actividades
+		# end
+		# setter
+		# def actividades (ololo)
+		#    @@actividades = ololo
+		# end		
 	end
 end
